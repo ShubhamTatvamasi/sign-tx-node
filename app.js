5c71138133dd2d295aeb2db5a0676486493aedd7
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-22 13:17:22 +0530

    new file:   .gitignore 	new file:   app.js 	new file:   contract.js 	new file:   package-lock.json 	new file:   package.json

---
+++ app.js
@@ -0,0 +1,37 @@
+const Web3 = require('web3')
+const EthereumTx = require('ethereumjs-tx');
+const contract = require('./contract')
+const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io"))
+const myContract = new web3.eth.Contract(contract.abi, contract.address)
+
+const PublicKey = "0xbe862AD9AbFe6f22BCb087716c7D89a26051f74C"
+const PrivateKey = "0xe331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109"
+
+// Update the message value for new message
+const message = 'Coffee Lake'
+
+const data = myContract.methods.updateMessage(message).encodeABI()
+
+web3.eth.getTransactionCount(PublicKey, (err, nonce) => {
+
+   const formatedPrivteKey = PrivateKey.substring(2);
+   const privateKey = Buffer.from(formatedPrivteKey, 'hex')
+
+   const txParams = {
+     nonce: nonce,
+     gasPrice: 1e9,
+     gasLimit: 1e6,
+     to: contract.address,
+     value: 0,
+     data: data
+   }
+
+   const tx = new EthereumTx(txParams)
+   tx.sign(privateKey)
+   const signedTX = "0x" + tx.serialize().toString('hex')
+    
+   web3.eth.sendSignedTransaction(signedTX, (error, result) => {
+     
+    console.log(result)
+   })
+})
---
+++ contract.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+  address: "0xf5d5175aa8c7ea16f370b1acef72daeb10586040",
+  abi: [{"constant":false,"inputs":[{"name":"_message","type":"string"}],"name":"updateMessage","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"message","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"string"}],"name":"newMessage","type":"event"}]
+
+}
